c2bb8814c511bbec6438fb0f2a4eb1cf8279da5d
Hmmm8938 <39926063+Hmmm-8938@users.noreply.github.com>
2024-10-22 22:15:35 -0600

    Week 6 Directory Created
    Will finish the updates tomorrow for this directory. Need to still add the sorting function. Might try redoing the Styling to make it look nicer as I don't think it looked great in week 3

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Home() {
         <li className="text-2xl text-cyan-600 hover:text-cyan-950"><Link href="./week-3">Week #3 Assignment</Link></li>
         <li className="text-2xl text-cyan-600 hover:text-cyan-950"><Link href="./week-4">Week #4 Assignment</Link></li>
         <li className="text-2xl text-cyan-600 hover:text-cyan-950"><Link href="./week-5">Week #5 Assignment</Link></li>
+        <li className="text-2xl text-cyan-600 hover:text-cyan-950"><Link href="./week-6">Week #6 Assignment (WIP)</Link></li>
       </ul>
     </main>
   );
