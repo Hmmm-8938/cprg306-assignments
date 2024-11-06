19b70bcd072ae523c73e76d967b3e253d4fc478b
Hmmm8938 <39926063+Hmmm-8938@users.noreply.github.com>
2024-11-05 19:21:48 -0700

    Updated Week 8 and Created Week 9 files.
    Will complete week 8 a little later. Just need to finish it up but need to review a bit of the material as I am getting some errors.

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -1,1 +1,59 @@
-// To EDIT.
+"use client"
+import {useEffect, useState } from "react";
+
+export default function MealIdeas()
+{
+    const [mealIDS, setMealIDS] = useState([]);
+    const [mealNames, setMealNames] = useState([]);
+    const [mealPhotos, setMealPhotos] = useState([]);
+
+    async function fetchMealIdeas(ingredient)
+    {
+        try
+        {
+            const response = await fetch(
+                `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+            );
+            if ( !response.ok ) console.log(response.statusText);
+            const data = await response.json();
+            console.log(data);
+        }
+        catch (error)
+        {
+            console.log(`Error: ${error.message}`);
+        }
+    }
+
+    function displayIdeas()
+    {
+
+    }
+
+    return(
+        <div onClick={closeFormFunc} className="absolute w-full h-full bg-gray-700/60 flex items-center justify-center z-10">
+            <form onClick={(event) => event.stopPropagation()} className="bg-green-400 text-black p-5 rounded-lg max-w-md"onSubmit={handleSubmit}>
+                <h2 className="text-2xl text-center mb-2">Add a new dog for adoption.</h2>
+                <div className="mb-2">
+                    <label className="inline-block w-40">ID:</label>
+                    <input type="text" value={id} onChange={handleIdChange}></input>
+                </div>
+                <div className="mb-2">
+                    <label className="inline-block w-40">Name:</label>
+                    <input type="text" value={name} onChange={(event) => setName(event.target.value)}></input>
+                </div>
+                <div className="mb-2">
+                    <label className="inline-block w-40">Breed:</label>
+                    <input type="text" value={breed} onChange={(event) => setBreed(event.target.value)}></input>
+                </div>
+                <div className="mb-2">
+                    <label className="inline-block w-40">Photo URL:</label>
+                    <input type="text" value={photoUrl} onChange={(event) => setPhotoUrl(event.target.value)}></input>
+                </div>
+                <button className="bg-green-700 text-white rounded px-1"type="submit">
+                Add New Dog
+                </button>
+                <button type="button" onClick={closeFormFunc} className="bg-blue-700 text-white rounded px-1 ml-3">Close Form</button>
+            </form>
+        </div>
+    )
+}
--- app/week-8/page.js
+++ app/week-8/page.js
@@ -4,11 +4,16 @@ import NewItem from "./new-item";
 import ItemList from "./item-list";
 import items from "./items.json"
 import { useState } from "react";
+import MealIdeas from "./meal-ideas";
 
-export default function Week7()
+export default function Week8()
 {
     let itemsArray = items.map((item) => ({...item}));
     const [itemsList, setItemsList] = useState(itemsArray)
+    const [showForm, setShowForm] = useState(false);
+
+    const openForm = () => setShowForm(true);
+    const closeForm = () => setShowForm(false);
 
     const addNewItem = (newItemObject) => 
     {
@@ -25,6 +30,10 @@ export default function Week7()
                 <div>
                     <ItemList listOfItems={itemsList}/>
                 </div>
+            </div>
+            {showForm && <MealIdeas addNewDogFunc={addNewItem} closeFormFunc={closeForm}/>}
+            <div className="absolute top-10 right-5 z-0 w-8 h-8 rounded-full overflow-hidden">
+                <button className="bg-blue-700 text-white text-3xl w-full h-full leading-none" type="button" onClick={openForm}>+</button>
             </div>
 
 
